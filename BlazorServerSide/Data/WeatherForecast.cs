96f4a43e1385cc4ff608f49e4e1358e14a789b41
chstorb <christian.storb@outlook.com>
2020-09-21 12:51:21 +0200

    event handling

--- BlazorServerSide/Data/WeatherForecast.cs
+++ BlazorServerSide/Data/WeatherForecast.cs
@@ -11,5 +11,7 @@ namespace BlazorServerSide.Data
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
         public string Summary { get; set; }
+
+        public bool Selected { get; set; }
     }
 }
