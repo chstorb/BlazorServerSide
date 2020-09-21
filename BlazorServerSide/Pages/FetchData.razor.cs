62ffb1fa90e9576e9a340e765e288f0cd0e208c9
chstorb <christian.storb@outlook.com>
2020-09-21 15:07:54 +0200

    Migrating @Code to Code-Behind

---
+++ BlazorServerSide/Pages/FetchData.razor.cs
@@ -0,0 +1,22 @@
+﻿using BlazorServerSide.Data;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerSide.Pages
+{
+    public partial class FetchData
+    {
+        [Inject]
+        public WeatherForecastService ForecastService { get; set; }
+
+        private WeatherForecast[] forecasts;
+
+        protected override async Task OnInitializedAsync()
+        {
+            forecasts = await ForecastService.GetForecastAsync(DateTime.Now);
+        }
+    }
+}
